2f179e6400ede0d8bc200d99e7229d5f484772a2
Ayushi Prajapati <ayushiprajapati021@gmail.com>
2025-06-24 22:43:41 +0530

    Basic route

--- Backend/routes/urlRoutes.js
+++ Backend/routes/urlRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const { shortenUrl, redirectUrl } = require('../controllers/urlController');
 
+router.get('/', (req, res) => res.send("Api is Running"))
 router.post('/api/shorten', shortenUrl);
 router.get('/:shortId', redirectUrl);
 
