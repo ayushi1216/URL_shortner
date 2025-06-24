9b43fdc54c290acb952c4f08c01cc30e132e5c2c
Ayushi Prajapati <129184173+ayushi1216@users.noreply.github.com>
2025-06-25 01:22:32 +0530

    Update urlRoutes.js

--- Backend/routes/urlRoutes.js
+++ Backend/routes/urlRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const { shortenUrl, redirectUrl } = require('../controllers/urlController');
+const { shortenUrl, redirectUrl, getUrls } = require('../controllers/urlController');
 
 router.get('/', (req, res) => res.send("Api is Running"))
 router.post('/api/shorten', shortenUrl);
+router.get(`/api/urls`, getUrls);
 router.get('/:shortId', redirectUrl);
 
 
